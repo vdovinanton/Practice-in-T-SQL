c1ee4876dcffc76798c429b5907e35c41942449a
antonvdovin92@gmail.com <antonvdovin92@gmail.com>
2018-01-09 15:41:31 +0200

    Add script with examples with select by range of data

---
+++ 2. Basic DWL/dbo.ExamplesGetRangeOfData.sql
@@ -0,0 +1,20 @@
+USE productsdb
+
+SELECT TOP 4 ProductName
+FROM Products
+
+/* GETS COUNT OF % */
+SELECT TOP 75 PERCENT ProductName
+FROM Products
+
+/* GETS ALL AFTER TWO ROWS */
+SELECT * FROM Products
+ORDER BY Id 
+    OFFSET 2 ROWS;
+
+/* GETS THREE AFTER TWO ROWS */
+SELECT * FROM Products
+ORDER BY Id 
+    OFFSET 2 ROWS
+    FETCH NEXT 3 ROWS ONLY;
+
