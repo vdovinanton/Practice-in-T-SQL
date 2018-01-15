bf43aeecad8cfa56dcb338d272adaff2c949c129
antonvdovin92@gmail.com <antonvdovin92@gmail.com>
2018-01-15 12:25:41 +0200

    Finished exercises in the Waregouses db examples

--- Exercises/Warehouse/dbo.Exercises.sql
+++ Exercises/Warehouse/dbo.Exercises.sql
@@ -44,7 +44,7 @@ SELECT Code, COUNT(*)
 
 --9. Select the codes of all warehouses that are saturated 
 --   (a warehouse is saturated if the number of boxes in it is larger than the warehouse's capacity).
-SELECT Warehouses.Code, Warehouses.[Location]
+SELECT Warehouses.Code, Warehouses.Location
     FROM Warehouses
     WHERE Warehouses.Capacity <
         (
@@ -53,6 +53,59 @@ SELECT Warehouses.Code, Warehouses.[Location]
                 WHERE Warehouse = Warehouses.Code
         )
 
+--10. Select the codes of all the boxes located in Chicago.
+SELECT Boxes.Code as BoxCode, Warehouses.Location
+    FROM Boxes
+    JOIN Warehouses ON Boxes.Warehouse = Warehouses.Code
+    WHERE convert(NVARCHAR(10) , Warehouses.Location) = 'Chicago'
 
+--11. Create a new warehouse in New York with a capacity for 3 boxes.
+INSERT INTO Warehouses(Code, Location, Capacity) VALUES(6,'New York', 3);
 
+--12. Create a new box, with code "H5RT", containing "Papers" with a value of $200, 
+--    and located in warehouse 2.
+INSERT INTO Boxes(Contents,Value,Warehouse) VALUES('Papers', 200 ,2);
 
+--13. Reduce the value of all boxes by 15%.
+SELECT  Code as BoxCode,
+        ROUND([Value] - (Value * 0.15), 0) as ReducedValue,
+        Value FROM Boxes
+UPDATE Boxes
+
+SET [Value] = ROUND([Value] - (Value * 0.15), 0)
+
+--14. Apply a 20% value reduction to boxes with a value larger than the average value of all the boxes.
+--14. Примените 20% -ное снижение стоимости к ящикам со значением, большим, чем среднее значение всех полей.
+SELECT Code, Value, ROUND([Value] - (Value * 0.20), 0)
+    FROM Boxes
+    WHERE [Value] > 
+        (
+            SELECT AVG(Value) 
+                FROM Boxes
+        )
+
+UPDATE Boxes
+SET Value = ROUND([Value] - (Value * 0.20), 0)
+    WHERE [Value] > 
+            (
+                SELECT AVG(Value) 
+                    FROM Boxes
+            )
+
+--15. Remove all boxes with a value lower than $100.
+DELETE Boxes
+    WHERE [Value] > 100
+
+--16**. Remove all boxes from saturated warehouses.
+DELETE FROM Boxes 
+    WHERE Warehouse IN
+   (
+     SELECT Code
+       FROM Warehouses
+       WHERE Capacity <
+       (
+         SELECT COUNT(*)
+           FROM Boxes
+           WHERE Warehouse = Warehouses.Code
+       )
+   );
